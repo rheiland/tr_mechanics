871ba7c9a75d8d45eb76f362f05858ee7785bd95
Furkan Kurtoglu <fkurtog@iu.edu>
2020-01-24 15:15:59 -0500

    Initial Commit

---
+++ data/user_params.py
@@ -0,0 +1,123 @@
+ 
+# This file is auto-generated from a Python script that parses a PhysiCell configuration (.xml) file.
+#
+# Edit at your own risk.
+#
+import os
+from ipywidgets import Label,Text,Checkbox,Button,HBox,VBox,FloatText,IntText,BoundedIntText,BoundedFloatText,Layout,Box
+    
+class UserTab(object):
+
+    def __init__(self):
+        
+        micron_units = Label('micron')   # use "option m" (Mac, for micro symbol)
+
+        constWidth = '180px'
+        tab_height = '500px'
+        stepsize = 10
+
+        #style = {'description_width': '250px'}
+        style = {'description_width': '25%'}
+        layout = {'width': '400px'}
+
+        name_button_layout={'width':'25%'}
+        widget_layout = {'width': '15%'}
+        units_button_layout ={'width':'15%'}
+        desc_button_layout={'width':'45%'}
+
+        param_name1 = Button(description='random_seed', disabled=True, layout=name_button_layout)
+        param_name1.style.button_color = 'lightgreen'
+
+        self.random_seed = IntText(
+          value=0,
+          step=1,
+          style=style, layout=widget_layout)
+
+        param_name2 = Button(description='cell_cell_adhesion_strength', disabled=True, layout=name_button_layout)
+        param_name2.style.button_color = 'tan'
+
+        self.cell_cell_adhesion_strength = FloatText(
+          value=0.4,
+          step=0.1,
+          style=style, layout=widget_layout)
+
+        param_name3 = Button(description='cell_cell_repulsion_strength', disabled=True, layout=name_button_layout)
+        param_name3.style.button_color = 'lightgreen'
+
+        self.cell_cell_repulsion_strength = FloatText(
+          value=10.0,
+          step=1,
+          style=style, layout=widget_layout)
+
+        param_name4 = Button(description='relative_maximum_adhesion_distance', disabled=True, layout=name_button_layout)
+        param_name4.style.button_color = 'tan'
+
+        self.relative_maximum_adhesion_distance = FloatText(
+          value=2.0,
+          step=0.1,
+          style=style, layout=widget_layout)
+
+        units_button1 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button1.style.button_color = 'lightgreen'
+        units_button2 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button2.style.button_color = 'tan'
+        units_button3 = Button(description='', disabled=True, layout=units_button_layout) 
+        units_button3.style.button_color = 'lightgreen'
+        units_button4 = Button(description='micron', disabled=True, layout=units_button_layout) 
+        units_button4.style.button_color = 'tan'
+
+        desc_button1 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button1.style.button_color = 'lightgreen'
+        desc_button2 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button2.style.button_color = 'tan'
+        desc_button3 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button3.style.button_color = 'lightgreen'
+        desc_button4 = Button(description='', disabled=True, layout=desc_button_layout) 
+        desc_button4.style.button_color = 'tan'
+
+        row1 = [param_name1, self.random_seed, units_button1, desc_button1] 
+        row2 = [param_name2, self.cell_cell_adhesion_strength, units_button2, desc_button2] 
+        row3 = [param_name3, self.cell_cell_repulsion_strength, units_button3, desc_button3] 
+        row4 = [param_name4, self.relative_maximum_adhesion_distance, units_button4, desc_button4] 
+
+        box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='100%')
+        box1 = Box(children=row1, layout=box_layout)
+        box2 = Box(children=row2, layout=box_layout)
+        box3 = Box(children=row3, layout=box_layout)
+        box4 = Box(children=row4, layout=box_layout)
+
+        self.tab = VBox([
+          box1,
+          box2,
+          box3,
+          box4,
+        ])
+
+    # Populate the GUI widgets with values from the XML
+    def fill_gui(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//user_parameters')  # find unique entry point
+        self.random_seed.value = int(uep.find('.//random_seed').text)
+        self.cell_cell_adhesion_strength.value = float(uep.find('.//cell_cell_adhesion_strength').text)
+        self.cell_cell_repulsion_strength.value = float(uep.find('.//cell_cell_repulsion_strength').text)
+        self.relative_maximum_adhesion_distance.value = float(uep.find('.//relative_maximum_adhesion_distance').text)
+
+
+    # Read values from the GUI widgets to enable editing XML
+    def fill_xml(self, xml_root):
+        uep = xml_root.find('.//microenvironment_setup')  # find unique entry point
+        vp = []   # pointers to <variable> nodes
+        if uep:
+            for var in uep.findall('variable'):
+                vp.append(var)
+
+        uep = xml_root.find('.//user_parameters')  # find unique entry point
+        uep.find('.//random_seed').text = str(self.random_seed.value)
+        uep.find('.//cell_cell_adhesion_strength').text = str(self.cell_cell_adhesion_strength.value)
+        uep.find('.//cell_cell_repulsion_strength').text = str(self.cell_cell_repulsion_strength.value)
+        uep.find('.//relative_maximum_adhesion_distance').text = str(self.relative_maximum_adhesion_distance.value)
